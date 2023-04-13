b909dff60561595ce8ff555566a0f83148650565
Derek Charlton <dc455@byu.edu>
2023-04-13 17:42:57 -0600

    scaffold!

---
+++ Models/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntexDatabaseConnection.Models
+{
+    public partial class Authentication
+    {
+        public long UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public bool Role { get; set; }
+    }
+}
